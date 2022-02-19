95f9ccece37087fa8e6eb99ffcf8cb2a6331bce6
tony <antolisah@gmail.com>
2022-02-19 15:13:17 +0300

    bottom tabs with translatey functionality

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -80,7 +80,7 @@ function Tab2Stack() {
     <Stack.Navigator>
       <Stack.Screen 
         name="Tab2" 
-        component={Tabs2Screen} 
+        component={BottomTab2} 
         options={{ headerShown: false }} 
       />
     </Stack.Navigator>
@@ -179,11 +179,87 @@ const TabButton = (props: any) => {
   )
 }
 
+const animate1 = {0: { scale: .5, translateY: 8 }, .92: { translateY: -34 }, 1: { scale: 1.2, translateY: -24 }};
+const animate2 = {0: { scale: 1.2, translateY: -24 }, 1: { scale: 1, translateY: 8 }};
+
+const circle1 = {0: { scale: 0 }, 0.3: { scale: .9 }, 0.5: { scale: .2 }, 0.8: { scale: .7 }, 1: { scale: 1 }};
+const circle2 = {0: { scale: 1 }, 1: { scale: 0 }};
+
+const TabButton2 = (props: any) => {
+  const { item, onPress, accessibilityState } = props
+  const focused = accessibilityState.selected;
+
+  const viewRef = useRef(null);
+  const circleRef = useRef(null);
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    if (focused) {
+      viewRef.current.animate(animate1)
+      circleRef.current.animate(circle1)
+      textRef.current.transitionTo({scale: 1})
+    } else {
+      viewRef.current.animate(animate2)
+      circleRef.current.animate(circle2)
+      textRef.current.transitionTo({scale: 0})
+    }
+  }, [focused]);
+
+  return(
+    <TouchableOpacity 
+      onPress={onPress}
+      activeOpacity={1}
+      style={styles.container}>
+      <Animatable.View
+        ref={viewRef}
+        duration={1000}
+        style={styles.container}>
+        <View style={styles.btn}>
+          <Animatable.View
+            ref={circleRef} 
+            style={styles.circle} />
+          <TabBarIcon 
+            name={focused ? item.activeIcon : item.inactiveIcon}
+            color={focused ? '#fff' : '#637aff49' }
+          />
+        </View>
+        <Animatable.Text
+          ref={textRef}
+          style={styles.text}>
+          {item.label}
+        </Animatable.Text>
+      </Animatable.View>
+    </TouchableOpacity>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  btn: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 4,
+    borderColor: '#fff',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  circle: {
+    ...StyleSheet.absoluteFillObject,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#637aff',
+    borderRadius: 25,
+  },
+  text: {
+    fontSize: 10,
+    color: '#637aff',
+    textAlign: 'center',
   },
 });
 
@@ -216,6 +292,43 @@ function BottomTab1() {
               tabBarShowLabel: false,
               // tabBarIcon: ({ color, focused }) => <TabBarIcon name={focused ? activeIcon : inactiveIcon} color={color} />,
               tabBarButton: (props) => <TabButton {...props} item={item} />
+            })}
+          />
+        )
+      })}
+    </BottomTab.Navigator>
+  );
+}
+
+function BottomTab2() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <BottomTab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme].tint,
+        headerShown: false,
+        tabBarStyle: {
+          height: 70,
+          position: 'absolute',
+          bottom: 16,
+          left: 16,
+          right: 16,
+          borderRadius: 20,
+        }
+      }}>
+      {TabArr.map((item) => {
+        const { name, label, activeIcon, inactiveIcon, component } = item
+        return (
+          <BottomTab.Screen
+            key={name}
+            name={name}
+            component={component}
+            options={({ navigation }) => ({
+              tabBarShowLabel: false,
+              // tabBarIcon: ({ color, focused }) => <TabBarIcon name={focused ? activeIcon : inactiveIcon} color={color} />,
+              tabBarButton: (props) => <TabButton2 {...props} item={item} />
             })}
           />
         )
